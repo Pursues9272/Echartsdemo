a4f504214b5ba0690b2ca1e166c738d1d76ee0f4
gk <hk_0815moon@163.com>
2022-08-31 15:26:02 +0800

    项目初始化

---
+++ src/views/IndexView.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="index"><el-button type="primary">Primary</el-button></div>
+  </template>
+
+<script>
+// @ is an alias to /src
+export default {
+  data () {
+    return {}
+  },
+  mounted () {
+    this.init()
+  },
+  methods: {
+    async init () {
+      const list = await this.axios.get('http://127.0.0.1:4523/m1/1278146-0-default/pet/2')
+      console.log(list)
+    }
+  }
+}
+</script>
+
+  <style lang="less" scoped>
+  // 严格模式下必须存在内容
+  </style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,30 @@
 const { defineConfig } = require('@vue/cli-service')
+const AutoImport = require('unplugin-auto-import/webpack')
+const Components = require('unplugin-vue-components/webpack')
+const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  publicPath: './',
+  transpileDependencies: true,
+  productionSourceMap: false, // 源码加密
+  devServer: {
+    port: 8088, // 端口号配置
+    open: false // 运行自启
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        components: '@/components',
+        views: '@/views'
+      }
+    },
+    plugins: [
+      AutoImport({
+        resolvers: [ElementPlusResolver()]
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()]
+      })
+    ]
+  }
 })
